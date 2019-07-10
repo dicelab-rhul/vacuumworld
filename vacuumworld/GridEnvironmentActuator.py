89cc6b9d2c4f528758947215b02fbc3a22d55815
NausheenSS <49731470+NausheenSS@users.noreply.github.com>
2019-07-10 11:04:46 +0100

    Update GridEnvironmentActuator.py

--- vacuumworld/GridEnvironmentActuator.py
+++ vacuumworld/GridEnvironmentActuator.py
@@ -2,7 +2,20 @@ from pystarworlds.Actuator import Actuator
 from GridWorldAction import ForwardMoveMentAction,MoveRightAction,MoveLeftAction,SpeakAction, CleanDirtAction,DropDirtAction,BroadcastAction
 
 
-class MovementActuator(Actuator):
+class PhysicalActuator(Actuator):
+    def __init__(self,l):
+      super().__init__(l)
+      self.firstattempt=True
+    def attempt(self, action):
+     if(self.firstattempt):
+       super().attempt(action)
+       self.firstattempt=False
+    
+    def act(self):
+       self.firstattempt=True
+       return super().act() 
+
+class MovementActuator(PhysicalActuator):
     def __init__(self):
       super().__init__([ForwardMoveMentAction,MoveRightAction,MoveLeftAction]) 
     
@@ -11,10 +24,10 @@ class CommunicationActuator(Actuator):
     def __init__(self):
       super().__init__([SpeakAction,BroadcastAction]) 
     
-class DropDirtActuator(Actuator):
+class DropDirtActuator(PhysicalActuator):
     def __init__(self):
       super().__init__([DropDirtAction]) 
  
-class CleaningDirtActuator(Actuator):
+class CleaningDirtActuator(PhysicalActuator):
     def __init__(self):
       super().__init__([CleanDirtAction]) 
