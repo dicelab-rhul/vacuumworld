a3f38f574b14f64f8a1f92bdff3b053ee9a4bb04
Ben Wilkins <brjw@hotmail.co.uk>
2019-08-14 18:13:31 +0100

    changes to todo.py

--- .circleci/todo.py
+++ .circleci/todo.py
@@ -8,7 +8,7 @@ Created on Wed Aug 14 13:42:09 2019
 
 import os,re,argparse
 
-TODO_FILE = '../TODO.txt'
+TODO_FILE = 'TODO.txt'
 TODO_REGEX = re.compile(r'.*#.*TODO.*')
 
 FILES = []
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("d")
 args = parser.parse_args()
 
-for d in  os.walk('../' + args.d):
+for d in  os.walk(args.d):
     FILES.extend([d[0] + '/' + f for f in d[2] if f.endswith('.py')])
 
 print(FILES)
@@ -26,9 +26,9 @@ with open(TODO_FILE, 'w') as g:
     for file in FILES: 
         with open(file, 'r') as f:
             for i, line in enumerate(f.readlines()):
-                line = line.strip() + '\n '
+                line = line.strip() + '\n'
                 if re.search(TODO_REGEX, line):
-                    g.write(':'.join([f.name[3:], str(i), line]))
+                    g.write(':'.join([f.name, str(i), ' ' + line]))
             
         
         
