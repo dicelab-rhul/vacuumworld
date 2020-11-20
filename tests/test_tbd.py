8e3db9c9ee3d794de062edf16a39a592d79ea7c1
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2020-11-20 15:11:24 +0000

    Added TODOs.

---
+++ tests/test_tbd.py
@@ -0,0 +1,7 @@
+# TODO: test the saveload module.
+# TODO: test the GUI.
+# TODO: test the VWEnvironment (how?).
+# TODO: test the action factories.
+# TODO: test the action executors.
+# TODO: integration testing (how?).
+# TODO: test the intended crashes (i.e., where we want VW to crash immediately on non-compliant (human) user input).
