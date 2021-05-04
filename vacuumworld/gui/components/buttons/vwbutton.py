781a297066cbf61a982ac440a5a5c498400859d9
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2021-05-04 01:45:02 +0100

    Better import logic for PIL.ImageTk.PhotoImage.

--- vacuumworld/gui/components/buttons/vwbutton.py
+++ vacuumworld/gui/components/buttons/vwbutton.py
@@ -1,5 +1,5 @@
 from typing import Callable
-from PIL import ImageTk
+from PIL.ImageTk import PhotoImage
 from tkinter import Button, Frame, Image
 
 from .vwtooltips import create_tooltip
@@ -7,8 +7,8 @@ from .vwtooltips import create_tooltip
 
 
 class VWButton():
-    def __init__(self, parent: Frame, config: dict, img: Image, fun, text: str=None, tip_text: str="") -> None:
-        self.__img: ImageTk.PhotoImage = ImageTk.PhotoImage(img)
+    def __init__(self, parent: Frame, config: dict, img: Image, fun: Callable, text: str=None, tip_text: str="") -> None:
+        self.__img: PhotoImage = PhotoImage(img)
         self.__fun: Callable = fun
         self.__button: Button = Button(parent, text = text, bd=0, font = config["root_font"], fg=config["buttons_fg_colour"],
                                  highlightthickness = 0, bg=config["bg_colour"], activebackground=config["bg_colour"],
@@ -34,8 +34,8 @@ class VWButton():
     def get_button(self) -> Button:
         return self.__button
 
-    def set_img(self, img: ImageTk.PhotoImage) -> None:
+    def set_img(self, img: PhotoImage) -> None:
         self.__img = img
 
-    def get_img(self) -> ImageTk.PhotoImage:
+    def get_img(self) -> PhotoImage:
         return self.__img
--- vacuumworld/gui/components/buttons/vwdifficultybutton.py
+++ vacuumworld/gui/components/buttons/vwdifficultybutton.py
@@ -1,5 +1,6 @@
 from typing import Callable, List
-from PIL import ImageTk, Image
+from PIL import Image
+from PIL.ImageTk import PhotoImage
 from tkinter import Frame, Image as Img
 
 from .vwbutton import VWButton
@@ -8,7 +9,7 @@ from .vwbutton import VWButton
 
 class VWDifficultyButton(VWButton):
     def __init__(self, parent: Frame, config: dict, img: Img, fun: Callable, tip_text: str) -> None:
-        self.__imgs: List[ImageTk.PhotoImage] = [ImageTk.PhotoImage(img)]
+        self.__imgs: List[PhotoImage] = [PhotoImage(img)]
         self.__imgs.extend([VWDifficultyButton.next_image(img, i * (255/(len(config["user_mind_levels"])-1))) for i in range(1, len(config["user_mind_levels"]))])
         super(VWDifficultyButton, self).__init__(parent=parent, config=config, img=img, fun=self.onclick, tip_text=tip_text)
         self.__difficulty: int = config["default_user_mind_level"]
@@ -17,14 +18,14 @@ class VWDifficultyButton(VWButton):
         self._rfun: Callable = fun
 
     @staticmethod
-    def next_image(img, red) -> ImageTk.PhotoImage:
+    def next_image(img, red) -> PhotoImage:
         img_dif: Image.Image = Image.new("RGB", img.size)
         img_dif.paste(img)
         rr, gg, bb = img_dif.split()
         rr = rr.point(lambda p: p + red)
         img_dif: Image.Image = Image.merge("RGB", (rr, gg, bb))
 
-        return ImageTk.PhotoImage(img_dif)
+        return PhotoImage(img_dif)
 
     def get_difficulty(self) -> int:
         return self.__difficulty
