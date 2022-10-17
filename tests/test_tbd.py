eeb33aa535a675953a44cfb7be0ee55f21a119b5
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2022-10-17 20:57:11 +0100

    Added tests for the arguments of run().

--- tests/test_tbd.py
+++ tests/test_tbd.py
@@ -1,3 +1,2 @@
 # TODO: test the GUI. (How?)
-# TODO: integration testing (how?).
-# TODO: test the intended crashes (i.e., where we want VW to crash immediately on non-compliant (human) user input).
+# TODO: integration testing with the GUI. (How?)
