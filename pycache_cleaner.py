fe9c36a2168b8eabf5683656fa919f27b69058d3
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2022-10-24 17:41:59 +0100

    Update pycache_cleaner.py to incude the pytest cache directories.

--- pycache_cleaner.py
+++ pycache_cleaner.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 from shutil import rmtree
+from typing import List
 
 import os
 
 
 def main() -> None:
-    pycache_dir_name: str = "__pycache__"
+    pycache_dir_names: List[str] = ["__pycache__", ".pytest_cache"]
 
     for dir, subdir, _ in os.walk(os.getcwd()):
-        if pycache_dir_name in subdir:
-            rmtree(os.path.join(dir, pycache_dir_name))
-        elif pycache_dir_name in dir:
-            rmtree(dir)
+        for pycache_dir_name in pycache_dir_names:
+            if pycache_dir_name in subdir:
+                rmtree(os.path.join(dir, pycache_dir_name))
+            elif pycache_dir_name in dir:
+                rmtree(dir)
 
 
 if __name__ == "__main__":
