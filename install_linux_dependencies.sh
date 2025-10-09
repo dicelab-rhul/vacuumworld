57fd724f317d04abe6a6b7de239eb213378543fd
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2025-10-09 13:46:50 +0100

    Enhanced the LLM API.

--- install_linux_dependencies.sh
+++ install_linux_dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install python3 python3-pip python-is-python3 virtualenvwrapper libcairo2-dev x11-xserver-utils -y
+sudo apt install python3 python3-pip python-is-python3 virtualenvwrapper libcairo2-dev x11-xserver-utils python3-tk tk-dev -y
